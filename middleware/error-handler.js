14d29fa26266a7ca62fbac828cb236bf70a9eb20
Phavoa <efemiayafavour@gmail.com>
2024-07-24 21:53:57 +0100

    chore: create the CRUD endpoints

---
+++ middleware/error-handler.js
@@ -0,0 +1,8 @@
+const {CustomApiError} = requie('../errors/custom-error')
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  console.log(err)
+  return res.status(err.status).json({msg:err.message})
+}
+
+module.exports = errorHandlerMiddleware;
