d1552f4c0e515c7c64bd569d483568817af8534a
Phavoa <efemiayafavour@gmail.com>
2024-07-26 05:32:08 +0100

    tempoary done with the tsk managemennt app

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,8 +1,9 @@
-const {CustomApiError} = requie('../errors/custom-error')
-
+const { CustomAPIError } = require('../errors/custom-error')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err)
-  return res.status(err.status).json({msg:err.message})
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message })
+  }
+  return res.status(500).json({ msg: 'Something went wrong, please try again' })
 }
 
-module.exports = errorHandlerMiddleware;
+module.exports = errorHandlerMiddleware
